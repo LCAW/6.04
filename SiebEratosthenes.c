43a584676bfee8c0626d9d61ad9c0ba7cdec73eb
Laura <120367965+LCAW@users.noreply.github.com>
2023-01-10 14:28:57 +0100

    Update SiebEratosthenes.c

--- SiebEratosthenes.c
+++ SiebEratosthenes.c
@@ -1,1 +1,15 @@
-
+#include <stdio.h>
+int main(){
+    int i,true,j,false;
+    int sieb[300];
+    
+  for (i=2;i<300;i++){
+        sieb[i]=i+1;
+        true=sieb[i];
+        
+        
+            if(sieb[i]=true){
+                printf("%d\n",true);
+            }else{}}
+    return 0;
+}
